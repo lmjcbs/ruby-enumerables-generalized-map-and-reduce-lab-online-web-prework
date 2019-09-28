2a8093623f8beb4c83ada27918ae4363877413ba
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-28 15:35:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,9 +11,8 @@ end
 
 def reduce(source_array, starting_point = 0)
   i = 0
-  total = starting_point
   while i < source_array.size
-    total = yield(total, source_array[i])
+    total = yield(total = starting_point, source_array[i])
     i += 1
   end
   total
