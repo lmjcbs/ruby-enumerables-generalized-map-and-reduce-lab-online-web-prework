304897c0039621787e753b53fc037039775f8d7a
Liam Jacobs <liamdavidjacobs@gmail.com>
2019-09-28 16:09:00 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,15 +9,17 @@ def map(source_array)
   new_array
 end
 
-source_array = [ false, nil, nil, nil, true]
-
 def reduce(source_array, starting_point = nil)
-  i = 0
+  if starting_point
+    total = starting_point
+    i = 0
+  else
+    total = source_array[0]
+    i = 1
+  end
   while i < source_array.size
     total = yield(total, source_array[i])
     i += 1
   end
   total += starting_point
 end
-
-reduce(source_array){|memo, n| memo || n}
